3362ac6530eceb487fbf1eac209b55e8d6296df5
Pecina Ondrej <pecina.ondrej@gmail.com>
2015-08-25 23:21:27 +0200

    Login form

--- app/Backend/Sign/DefaultPresenter.php
+++ app/Backend/Sign/DefaultPresenter.php
@@ -23,9 +23,13 @@ class DefaultPresenter extends Presenter
 
 	protected function createComponentLoginForm()
 	{
+		$presenter = $this;
 		$form = $this->iLoginControlFactory->create();
-		$form->setCallback(function($name, $password) {
-			$this->getUser()->login($name, $password);
+		$form->setCallback(function($name, $password) use ($presenter) {
+			$presenter->getUser()->login($name, $password);
+			if ($presenter->getUser()->isLoggedIn()) {
+				$presenter->redirect(':Backend:Homepage:');
+			}
 		});
 
 		return $form;
